95c8325022358cd8ab90774a37da228392d6b45d
Nelson <nb.mejia@intsuperior.edu.ec>
2022-02-23 20:18:35 -0500

    Este es el Proyecto se Realiza con Cobexión a Base de Datos.

---
+++ Proyecto_SGA/src/ec/edu/intsuperior/controlador/Controlador.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import ec.edu.intsuperior.vista.*;
+
+/**
+ *
+ * @author Nelson
+ */
+public class Controlador {
+    
+    Administrador administrador=new Administrador(this);
+    Estudiante estudiante= new Estudiante(this);
+    Loggin loggin= new Loggin(this);
+    Docente docente=new Docente(this);
+    
+    public void mostrarDocente(){
+        docente.setVisible(true);
+    }
+    
+    public void mostrarAdministrador(){
+        administrador.setVisible(true);
+    }
+    
+    public void mostrarEstudiante(){
+        estudiante.setVisible(true);
+    }
+    
+    public void mostrarLoggin(){
+        loggin.setVisible(true);
+    }
+    
+    
+    //DDL: LENGUAJE DE DEFINICION DE DATOS (CREATE, ALTER, DROP)
+    
+    //DML: LENGUAJE DE MANIPULACION DE DATOS (SELECT, DELETE, UPDATE, INSERT)
+    
+    //CRUD
+    //CREATE
+    //READ
+    //UPDATE
+    //DELETE
+    
+}
---
+++ Proyecto_SGA/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.controlador.Conexion;
+import ec.edu.intsuperior.controlador.Controlador;
+import ec.edu.intsuperior.modelo.Arreglo;
+
+
+/**
+ *
+ * @author Nelson
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+     Controlador c= new Controlador();
+     c.mostrarLoggin();
+    }
+    
+}
